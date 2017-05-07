0c2d9acd9c7ea7ecd633757325dd7c9318de5797
Sebastian Bauer <mail@sebastianbauer.info>
2017-05-07 12:51:49 +0200

    Use data file that is part of the classpath.
    Extract the file if it is a jar. We could use InputStream but for now we
    don't support it directly. Also, we actual want to measure the time needed
    to parse the file from disk.

--- ontologizerlib-benchmark/src/main/java/ontologizer/OBOParserBenchmark.java
+++ ontologizerlib-benchmark/src/main/java/ontologizer/OBOParserBenchmark.java
@@ -1,21 +1,52 @@
 package ontologizer;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 import java.util.concurrent.TimeUnit;
 
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.Fork;
 import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.Warmup;
 
 import ontologizer.io.obo.OBOParser;
 import ontologizer.io.obo.OBOParserException;
 import ontologizer.io.obo.OBOParserFileInput;
 
+@State(Scope.Benchmark)
 public class OBOParserBenchmark
 {
-	// FIXME: Bundle that resource
-	private static final String obofile = "../../ontologizerlib-io/src/test/resources/gene_ontology.1_2.obo.gz";
+	private static final String OBO_NAME = "gene_ontology.1_2.obo.gz";
+
+	private static String oboFilename;
+
+	static
+	{
+		/* Determine an oboFilename that can be used to directly read from the filesystem */
+		try
+		{
+			ClassLoader cl = OBOParserBenchmark.class.getClassLoader();
+			oboFilename = cl.getResource(OBO_NAME).getPath();
+			if (oboFilename.contains("!/"))
+			{
+				/* The requested file is inside an archive, copy the contents to a temporary file */
+				InputStream is = cl.getResourceAsStream(OBO_NAME);
+				File tmpFile = File.createTempFile("obofile", ".obo.gz");
+				tmpFile.deleteOnExit();
+				Files.copy(is, tmpFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+				oboFilename = tmpFile.getAbsolutePath();
+			}
+		} catch (IOException e)
+		{
+			e.printStackTrace();
+			System.exit(-1);
+		}
+	}
 
 	@Benchmark
 	@Warmup(iterations=5)
@@ -23,7 +54,7 @@ public class OBOParserBenchmark
 	@Measurement(time=2,timeUnit=TimeUnit.SECONDS)
 	public OBOParser benchmarkOBOParser() throws IOException, OBOParserException
 	{
-		OBOParser oboParser = new OBOParser(new OBOParserFileInput(obofile));
+		OBOParser oboParser = new OBOParser(new OBOParserFileInput(oboFilename));
 		oboParser.doParse();
 		return oboParser;
 	}
