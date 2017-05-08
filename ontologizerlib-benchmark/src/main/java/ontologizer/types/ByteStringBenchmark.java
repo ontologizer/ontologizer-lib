1c6f82e6abbac2cc3d1afd200261e10c0c0b3649
Sebastian Bauer <mail@sebastianbauer.info>
2017-05-08 21:50:20 +0200

    Add benchmarks for ByteString.split().

--- ontologizerlib-benchmark/src/main/java/ontologizer/types/ByteStringBenchmark.java
+++ ontologizerlib-benchmark/src/main/java/ontologizer/types/ByteStringBenchmark.java
@@ -13,4 +13,20 @@ public class ByteStringBenchmark
 	{
 		return BS.toString();
 	}
+
+	private static final byte PIPE = (byte)'|';
+	private static final ByteString TO_SPLIT_1 = b("str1|str2|str3|str4|str5");
+	private static final ByteString TO_SPLIT_2 = b("str1|str2|str3|str4|str5|str6|str7|str8|str9|str10");
+
+	@Benchmark
+	public ByteString [] benchmarkSplitOf1()
+	{
+		return TO_SPLIT_1.split(PIPE);
+	}
+
+	@Benchmark
+	public ByteString [] benchmarkSplitOf2()
+	{
+		return TO_SPLIT_2.split(PIPE);
+	}
 }
