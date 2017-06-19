bcf13cdb70970c6ec5f54945e75b7699c5c9b1d0
Sebastian Bauer <mail@sebastianbauer.info>
2017-06-19 20:19:25 +0200

    Add fast path to create Ontologies from OBO file.

---
+++ ontologizerlib-io/src/main/java/ontologizer/io/obo/OBOOntologyCreator.java
@@ -0,0 +1,48 @@
+package ontologizer.io.obo;
+
+import java.io.File;
+import java.io.IOException;
+
+import ontologizer.io.ParserFileInput;
+import ontologizer.ontology.Ontology;
+import ontologizer.ontology.TermContainer;
+
+/**
+ * This class provides easy to use methods to create an
+ * ontology from an obo file. No fancy interactions are supported.
+ *
+ * @author Sebastian Bauer
+ */
+public class OBOOntologyCreator
+{
+	/**
+	 * Create an ontology from obo file.
+	 *
+	 * @param oboFile the file object describing attributes of the file to be parsed
+	 * @return the ontology object
+	 *
+	 * @throws IOException
+	 * @throws OBOParserException
+	 */
+	public static Ontology create(File oboFile) throws IOException, OBOParserException
+	{
+		return create(oboFile.getAbsolutePath());
+	}
+
+	/**
+	 * Create an ontology from a file that is accessible via a local path.
+	 *
+	 * @param oboPath the local path of the ontology.
+	 * @return the ontology object
+	 *
+	 * @throws IOException
+	 * @throws OBOParserException
+	 */
+	public static Ontology create(String oboPath) throws IOException, OBOParserException
+	{
+		OBOParser parser = new OBOParser(new ParserFileInput(oboPath));
+		parser.doParse();
+		TermContainer termContainer = new TermContainer(parser.getTermMap(), parser.getFormatVersion(), parser.getDate());
+		return Ontology.create(termContainer);
+	}
+}
