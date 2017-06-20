7d4714a9a7a3cd203df5f2bb932e449274076372
Sebastian Bauer <mail@sebastianbauer.info>
2017-06-20 23:27:50 +0200

    Introduce TestBase.
    This, for now, provides methods to extract Java
    source.

---
+++ ontologizerlib-test-helper/src/main/java/ontologizer/TestBase.java
@@ -0,0 +1,30 @@
+package ontologizer;
+
+import java.io.IOException;
+
+import org.junit.Rule;
+import org.junit.rules.TestName;
+
+import sonumina.test.TestClass;
+
+public class TestBase
+{
+	@Rule
+	public TestName testName = new TestName();
+
+	@Rule
+	public TestClass testClass = new TestClass();
+
+	/**
+	 * Return the path to a file that contains the comment of the current test.
+	 *
+	 * @return an absolute path.
+	 *
+	 * @throws IOException
+	 */
+	protected String getTestCommentAsPath() throws IOException
+	{
+		return TestSourceUtils.getCommentOfTestAsTmpFilePath(testClass.getTestClass(),
+			testName.getMethodName());
+	}
+}
