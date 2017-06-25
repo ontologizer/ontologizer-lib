530904a0e92bb0d49257759b7e821c0aafe39be3
Sebastian Bauer <mail@sebastianbauer.info>
2017-06-25 09:24:09 +0200

    Add TermPropertyMap.
    This can be used to access terms via specified properties.

---
+++ ontologizerlib-core/src/main/java/ontologizer/ontology/TermPropertyMap.java
@@ -0,0 +1,77 @@
+package ontologizer.ontology;
+
+import java.io.Serializable;
+import java.util.Collection;
+
+import sonumina.collections.IntMapper;
+import sonumina.collections.Map;
+import sonumina.collections.ObjectIntHashMap;
+
+/**
+ * Maps an arbitrary property to a given term. If multiple
+ * keys map to different terms, only the first one is tracked.
+ *
+ * @param <K> the key type of the property.
+ * @author Sebastian Bauer
+ */
+public class TermPropertyMap<K> implements Serializable
+{
+	private static final long serialVersionUID = 1L;
+
+	private ObjectIntHashMap<K> keyMap;
+	private TermMap termMap;
+
+	private int ambiguities;
+
+	/**
+	 * Construct a term property map that maps arbitrary keys to terms.
+	 *
+	 * @param termMap the term map.
+	 * @param map the map that specifies with keys should later map
+	 *  to the given term.
+	 */
+	public TermPropertyMap(TermMap termMap, Map<Term, Collection<K>> map)
+	{
+		keyMap = new ObjectIntHashMap<K>();
+		this.termMap = termMap;
+
+		IntMapper<TermID> termIDMapper = termMap.getTermIDMapper();
+		for (int i = 0; i < termIDMapper.getSize(); i++)
+		{
+			Term t = termMap.get(i);
+			Collection<K> keys = map.map(t);
+
+			for (K key : keys)
+			{
+				int newIndex = keyMap.getIfAbsentPut(key, i);
+				if (i != newIndex)
+				{
+					/* Key was already present, count up ambiguities */
+					ambiguities++;
+				}
+			}
+		}
+	}
+
+	/**
+	 * @return term id that is associated to a term with the given key value
+	 *  or null, if no such term exists.
+	 */
+	public TermID get(K key)
+	{
+		int index = keyMap.getIfAbsent(key, -1);
+		if (index == -1)
+		{
+			return null;
+		}
+		return termMap.getTermIDMapper().get(index);
+	}
+
+	/**
+	 * @return the number of ambiguities.
+	 */
+	public int getAmbiguities()
+	{
+		return ambiguities;
+	}
+}
---
+++ ontologizerlib-core/src/test/java/ontologizer/ontology/TermPropertyMapTest.java
@@ -0,0 +1,49 @@
+package ontologizer.ontology;
+
+import static ontologizer.ontology.TermID.tid;
+import static ontologizer.types.ByteString.b;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import sonumina.collections.Map;
+
+public class TermPropertyMapTest
+{
+	private Term t0;
+	private Term t1;
+	private Term t2;
+	private TermContainer container;
+
+	@Before
+	public void setUp() throws Exception
+	{
+		t0 = Term.name("root").id("T:0000000").altId(tid("T:1000000")).build();
+		t1 = Term.name("root").id("T:0000001").altId(tid("T:1000001")).build();
+		t2 = Term.name("root").id("T:0000002").altId(tid("T:1000002")).build();
+
+		HashSet<Term> termsConstructed = new HashSet<Term>(Arrays.asList(t0, t1, t2));
+		container = new TermContainer(termsConstructed, b("noformat"), b("nodate"));
+	}
+
+	@Test
+	public void testTermPropertyMap()
+	{
+		TermPropertyMap<TermID> altIdMap = new TermPropertyMap<TermID>(container, new Map<Term,Collection<TermID>>()
+		{
+			@Override
+			public Collection<TermID> map(Term key)
+			{
+				return Arrays.asList(key.getAlternatives());
+			}
+		});
+		Assert.assertEquals(tid("T:0000000"),altIdMap.get(tid("T:1000000")));
+		Assert.assertEquals(tid("T:0000001"),altIdMap.get(tid("T:1000001")));
+		Assert.assertEquals(tid("T:0000002"),altIdMap.get(tid("T:1000002")));
+	}
+}
